f6c00ddcfb615160d3f181cff31dbe3f97f2cf47
Vafa Karamzadegan <63124707+vafakaramzadegan@users.noreply.github.com>
2020-07-06 01:19:53 -0700

    Create HSV.py

---
+++ Python/Plotting Julia Set/HSV.py
@@ -0,0 +1,58 @@
+def HSV_2_RGB(HSV):
+    ''' Converts an integer HSV tuple (value range from 0 to 255) to an RGB tuple '''
+
+    # Unpack the HSV tuple for readability
+    H, S, V = HSV
+
+    # Check if the color is Grayscale
+    if S == 0:
+        R = V
+        G = V
+        B = V
+        return (R, G, B)
+
+    # Make hue 0-5
+    region = H // 43;
+
+    # Find remainder part, make it from 0-255
+    remainder = (H - (region * 43)) * 6; 
+
+    # Calculate temp vars, doing integer multiplication
+    P = (V * (255 - S)) >> 8;
+    Q = (V * (255 - ((S * remainder) >> 8))) >> 8;
+    T = (V * (255 - ((S * (255 - remainder)) >> 8))) >> 8;
+
+
+    # Assign temp vars based on color cone region
+    if region == 0:
+        R = V
+        G = T
+        B = P
+
+    elif region == 1:
+        R = Q; 
+        G = V; 
+        B = P;
+
+    elif region == 2:
+        R = P; 
+        G = V; 
+        B = T;
+
+    elif region == 3:
+        R = P; 
+        G = Q; 
+        B = V;
+
+    elif region == 4:
+        R = T; 
+        G = P; 
+        B = V;
+
+    else: 
+        R = V; 
+        G = P; 
+        B = Q;
+
+
+    return (R, G, B)
